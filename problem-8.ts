c05b1cd6f9bdabd60eca5564d19b5f3a557fdbfa
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-11-08 18:55:04 +0600

    problem 8 solved

--- problem-8.ts
+++ problem-8.ts
@@ -1,16 +1,18 @@
 {
     //Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
 
-    function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
-        const objKeys = obj;
+    function validateKeys<T extends object>(obj: T, keys: string[]) {
         const keysArr = keys;
-        console.log(objKeys , keysArr)
-
-        
-
-        return true;
+        const objKeys = Object.keys(obj);
+        let terminated = true;
+        keysArr.forEach((key) => {
+            if(!objKeys.includes(key)){
+                terminated = false;
+            }
+        })
+        return terminated;
     }
 
-    const person = { name: "Alice", age: 25, email: "alice@example.com" };
-    console.log(validateKeys(person, ["name", "age"]));
+    // const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    // console.log(validateKeys(person, ["name", "age"]));
 }
